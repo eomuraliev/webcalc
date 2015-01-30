9a1f34761bde12b6b5cbb6016be6eb48b57bea63
Eldar Omuraliev <eldar.omuraliev@livingsocial.com>
2015-01-29 23:10:39 -0500

    Remove console log statements from the server controller

--- server/api/calculate/calculate.controller.js
+++ server/api/calculate/calculate.controller.js
@@ -19,47 +19,37 @@ exports.index = function(req, res) {
     }
   }
    */
-  console.log(req.body);
   var operation = req.body.operation,
       currentValue = req.body.currentValue,
       storedValue = req.body.storedValue;
 
   if (isValidRequest()) {
-    console.log('passed isValidRequest');
     currentValue = new Big(currentValue);
     if (isPresent(storedValue)) {
       storedValue = new Big(storedValue);
     }
     switch (operation) {
       case 'add':
-        console.log('processing add');
         return okResponse(storedValue.plus(currentValue));
       case 'subtract':
-        console.log('processing subtract');
         return okResponse(storedValue.minus(currentValue));
       case 'multiply':
-        console.log('processing multiply');
         return okResponse(storedValue.times(currentValue));
       case 'divide':
-        console.log('processing divide');
         return okResponse(storedValue.div(currentValue));
       case 'percentage':
         if (storedValue instanceof Big) {
-          console.log('processing binary percentage');
           return okResponse( (storedValue.div(new Big(100))).times(currentValue) );
         }
         else {
-          console.log('processing unary percentage');
           return okResponse( currentValue.div(new Big(100)) );
         }
         break;
       default:
-        console.log('returning default error because no known op was detected');
         return invalidRequestResponse();
     }
   }
   else {
-    console.log('failed isValidRequest');
     return invalidRequestResponse();
   }
 
@@ -71,15 +61,12 @@ exports.index = function(req, res) {
   }
 
   function isValidRequest() {
-    console.log('isValidRequest: ', operation, currentValue, storedValue);
-    console.log(operationMap);
     return isPresent(operation) &&
            isPresent(currentValue) &&
            operation in operationMap &&
       (operationMap[operation].unary || isPresent(storedValue));
   }
   function isPresent(value) {
-    console.log(typeof value === 'string' && value !== '');
     return typeof value === 'string' && value !== '';
   }
 };
