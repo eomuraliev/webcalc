6cba37aab111034a931a90c276b50800cca0407e
Eldar Omuraliev <eldar.omuraliev@livingsocial.com>
2015-01-30 22:04:37 -0500

    Unit tests for equals functionality

--- client/app/interpreter/interpreter.service.spec.js
+++ client/app/interpreter/interpreter.service.spec.js
@@ -3,7 +3,8 @@
 describe('interpreter service', function() {
   var $injector,
       interpreter,
-      memory;
+      memory,
+      API_URL= '/api/calculate';
 
   beforeEach(module('CalculatorApp'));
   beforeEach(inject(function(_$injector_) {
@@ -74,5 +75,168 @@ describe('interpreter service', function() {
       });
     });
 
+    describe('equals', function() {
+      it('should not alter the current value when no operation is present', function() {
+        memory.setCurrentValue('5');
+        spyOn(memory, 'setCurrentValue');
+        interpreter.operation('equals');
+        expect(memory.setCurrentValue).not.toHaveBeenCalled();
+        expect(memory.currentValue).toBe('5');
+      });
+
+      it('after using equals, typing new keys should start a new value', function() {
+        memory.setCurrentValue('5');
+        spyOn(memory, 'setCurrentValue');
+        interpreter.operation('equals');
+
+        interpreter.entry('7');
+        expect(memory.setCurrentValue).toHaveBeenCalledWith('7');
+      });
+
+      describe('there is a currentValue, a storedValue, and an operation stored on memory', function() {
+        var $httpBackend;
+        beforeEach(function() {
+          $httpBackend = $injector.get('$httpBackend');
+          memory.setCurrentValue('55.5');
+          memory.setStoredValue('.5');
+          memory.setOperation('add');
+        });
+        afterEach(function() {
+          $httpBackend.verifyNoOutstandingExpectation();
+        });
+
+        it('makes a backend request', function() {
+          $httpBackend.expectPOST(API_URL, {
+            currentValue: '55.5',
+            storedValue: '.5',
+            operation: 'add'
+          }).respond({
+            result: '60'
+          });
+          interpreter.operation('equals');
+          $httpBackend.flush();
+        });
+
+        it('sets currentValue to the result and calls clearAll() without arguments', function() {
+          spyOn(memory, 'clearAll');
+          spyOn(memory, 'setCurrentValue');
+          $httpBackend.expectPOST(API_URL, {
+            currentValue: '55.5',
+            storedValue: '.5',
+            operation: 'add'
+          }).respond({
+            result: '60'
+          });
+          interpreter.operation('equals');
+          $httpBackend.flush();
+
+          expect(memory.clearAll).toHaveBeenCalledWith();
+          expect(memory.setCurrentValue).toHaveBeenCalledWith('60');
+        });
+
+        it('locks the interpreter after making a request and unlocks it when response is received', function() {
+          $httpBackend.expectPOST(API_URL, {
+            currentValue: '55.5',
+            storedValue: '.5',
+            operation: 'add'
+          }).respond({
+            result: '60'
+          });
+          interpreter.operation('equals');
+          expect(interpreter.isLocked()).toBe(true);
+          $httpBackend.flush();
+          expect(interpreter.isLocked()).toBe(false);
+        });
+
+        it('repeats the last operation on the currentValue if equal is pressed again', function() {
+          $httpBackend.expectPOST(API_URL, {
+            currentValue: '55.5',
+            storedValue: '.5',
+            operation: 'add'
+          }).respond({
+            result: '60'
+          });
+          interpreter.operation('equals');
+          $httpBackend.flush();
+
+          interpreter.operation('equals');
+          $httpBackend.expectPOST(API_URL, {
+            currentValue: '55.5',
+            storedValue: '60',
+            operation: 'add'
+          }).respond({
+            result: '115.5'
+          });
+
+          $httpBackend.flush();
+        });
+
+        it('repeats the last operation on the currentValue even if it was changed by a new entry', function() {
+          $httpBackend.expectPOST(API_URL, {
+            currentValue: '55.5',
+            storedValue: '.5',
+            operation: 'add'
+          }).respond({
+            result: '60'
+          });
+          interpreter.operation('equals');
+          $httpBackend.flush();
+
+          interpreter.entry('9');
+          interpreter.operation('equals');
+
+          $httpBackend.expectPOST(API_URL, {
+            currentValue: '55.5',
+            storedValue: '9',
+            operation: 'add'
+          }).respond({
+            result: '64.5'
+          });
+
+          $httpBackend.flush();
+        });
+
+        it('repeats the last operation on the currentValue even if a `clear` op was invoked on the currentValue', function() {
+          $httpBackend.expectPOST(API_URL, {
+            currentValue: '55.5',
+            storedValue: '.5',
+            operation: 'add'
+          }).respond({
+            result: '60'
+          });
+          interpreter.operation('equals');
+          $httpBackend.flush();
+
+          interpreter.operation('clear');
+          interpreter.operation('equals');
+          $httpBackend.expectPOST(API_URL, {
+            currentValue: '55.5',
+            storedValue: '0',
+            operation: 'add'
+          }).respond({
+            result: '55.5'
+          });
+          $httpBackend.flush();
+        });
+
+        it('does not repeat the last operation on the currentValue even if a `all_clear` op was invoked on the currentValue', function() {
+          $httpBackend.expectPOST(API_URL, {
+            currentValue: '55.5',
+            storedValue: '.5',
+            operation: 'add'
+          }).respond({
+            result: '60'
+          });
+          interpreter.operation('equals');
+          $httpBackend.flush();
+
+          interpreter.operation('all_clear');
+          interpreter.operation('equals');
+
+          $httpBackend.verifyNoOutstandingRequest();
+        });
+      });
+
+    });
   });
 });
