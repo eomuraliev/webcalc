3d36ee68de2578d8a3e8d7c73cf2e7a57e9768ec
Eldar Omuraliev <eldar.omuraliev@livingsocial.com>
2015-02-02 00:07:46 -0500

    More interpreter service specs

--- client/app/interpreter/interpreter.service.spec.js
+++ client/app/interpreter/interpreter.service.spec.js
@@ -238,5 +238,107 @@ describe('interpreter service', function() {
       });
 
     });
+
+    describe('binary operations (add, subtract, multiply, divide)', function() {
+      var $httpBackend;
+      beforeEach(function() {
+        $httpBackend = $injector.get('$httpBackend');
+      });
+
+      afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+      });
+
+      it('should execute the existing operation when storedValue, currentValue, and existing Value are present', function() {
+        memory.setCurrentValue('3');
+        memory.setStoredValue('3');
+        memory.setOperation('multiply');
+        $httpBackend.expectPOST(API_URL, {
+          currentValue: '3',
+          storedValue: '3',
+          operation: 'multiply'
+        }).respond({
+          result: '9'
+        });
+        interpreter.operation('add');
+        expect(interpreter.isLocked()).toBe(true);
+        $httpBackend.flush();
+
+        expect(interpreter.isLocked()).toBe(false);
+        expect(memory.currentValue).toBe('9');
+        expect(memory.operation).toBe('add');
+      });
+
+      it('a new entry should save to storedValue, and start a new currentValue, after an operation has been picked', function() {
+        interpreter.entry('3');
+        interpreter.operation('add');
+        interpreter.entry('7');
+
+        expect(memory.currentValue).toBe('7');
+        expect(memory.storedValue).toBe('3');
+        expect(memory.operation).toBe('add');
+      });
+
+      it('a new entry should save to storedValue, and start a new currentValue, even after the new operation executes an existing one', function() {
+        memory.setCurrentValue('3');
+        memory.setStoredValue('3');
+        memory.setOperation('subtract');
+        $httpBackend.expectPOST(API_URL, {
+          currentValue: '3',
+          storedValue: '3',
+          operation: 'subtract'
+        }).respond({
+          result: '0'
+        });
+        interpreter.operation('divide');
+        $httpBackend.flush();
+        interpreter.entry('4');
+
+        expect(memory.currentValue).toBe('4');
+        expect(memory.storedValue).toBe('0');
+        expect(memory.operation).toBe('divide');
+      });
+    });
+
+    describe('percentage operation', function() {
+      var $httpBackend;
+      beforeEach(function() {
+        $httpBackend = $injector.get('$httpBackend');
+      });
+
+      afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+      });
+
+      it('should calculate the percentage just with the currentValue, if there is no storedValue', function() {
+        interpreter.entry('5');
+        interpreter.operation('percentage');
+        $httpBackend.expectPOST(API_URL, {
+          currentValue: '5',
+          operation: 'percentage'
+        }).respond({
+          result: '0.05'
+        });
+        $httpBackend.flush();
+        expect(memory.currentValue).toBe('0.05');
+      });
+
+      it('should calculate the percentage based on the storedValue, if there is one present', function() {
+        interpreter.entry('50');
+        interpreter.operation('add');
+        interpreter.entry('5');
+        interpreter.operation('percentage');
+        $httpBackend.expectPOST(API_URL, {
+          currentValue: '5',
+          storedValue: '50',
+          operation: 'percentage'
+        }).respond({
+          result: '0.1'
+        });
+        $httpBackend.flush();
+        expect(memory.currentValue).toBe('0.1');
+      });
+    });
+
   });
 });
